84104284ea02225e76ba5223aa9f3b53da6c0496
uppi28 <upendrabr.93@gmail.com>
2019-06-27 01:14:22 +0530

    ng-content update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a Test!'}]; 
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a Test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
